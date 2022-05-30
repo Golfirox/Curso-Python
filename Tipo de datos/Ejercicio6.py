84cec0eabffa232295ba507e8f60385914885d5b
Golfirox <106554979+Golfirox@users.noreply.github.com>
2022-05-30 21:15:36 +0200

    Add files via upload

---
+++ Tipo de datos/Ejercicio6.py
@@ -0,0 +1,5 @@
+#Realiza un programa que reciba una cantidad de minutos y muestre por pantalla a cuantas  horas y minutos corresponde.
+a =int (input ("Dime un numero "))
+b =int (a/60)
+c =int (a%60)
+print (b,"horas","y", c,"minutos")
