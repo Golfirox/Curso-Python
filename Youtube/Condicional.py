014e0b8fcdb99fb68bbf84ff11e5d981f886a597
Golfirox <106554979+Golfirox@users.noreply.github.com>
2022-05-30 21:21:41 +0200

    Add files via upload

---
+++ Youtube/Condicional.py
@@ -0,0 +1,12 @@
+numero=int (input("Dime un número: "))
+if numero>0:
+  print ("es positivo")
+elif numero ==0:
+  print ("el numero es Cero")
+else:
+  print ("es negativo")
+
+print ("Fin del programa")
+
+
+  
