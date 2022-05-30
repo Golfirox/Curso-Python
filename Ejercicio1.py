f265b0572e115458656279b5436b67226bbb751f
Golfirox <106554979+Golfirox@users.noreply.github.com>
2022-05-30 21:08:58 +0200

    Add files via upload

---
+++ Ejercicio1.py
@@ -0,0 +1,3 @@
+#Escribir un programa que pregunte al usuario su nombre, y luego lo salude
+a=input ("Su nombre ")
+print ("Hola", a)
---
+++ Ejercicio5.py
@@ -0,0 +1,3 @@
+#Escribir un programa que le pida una palabra al usuario, para luego imprimirla 1000 veces,  con espacios intermedios.
+a=input ("Dime una palabra ")
+print ((a+(" "))*1000)
