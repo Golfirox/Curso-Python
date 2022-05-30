0a928b2b7628f6ffd5f9f8ed6193f9d3fd394859
Golfirox <106554979+Golfirox@users.noreply.github.com>
2022-05-30 21:10:34 +0200

    Delete Ejercicio3.py

--- Ejercicio3.py
+++
@@ -1,8 +0,0 @@
-#3.	Calcular el perímetro y área de un círculo dado su radio.
-print ("Vamos a calcular el area y el perimetro de un circulo")
-r =int (input ("Cuanto mide el radio "))
-pi = float (3.14)
-A = float (pi*r**2)
-P = float (2*pi*r)
-print ("El area es de ", A)
-print ("El perimetro es de ", P)
