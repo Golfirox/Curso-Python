d314cfb944b770bbd75bd70097fe9124cb6cd6ea
Golfirox <106554979+Golfirox@users.noreply.github.com>
2022-05-30 21:10:23 +0200

    Delete Ejercicio2.py

--- Ejercicio2.py
+++
@@ -1,8 +0,0 @@
-#Calcular el perímetro y área de un rectángulo dada su base y su altura. Calcular también el  perímetro y área de un triangulo.  
-print ("Vamos a calcular el area y el perimetro de un rectangulo ")
-l=int (input ("Dime cuanto mide un lado "))
-a=int (input ("Dime su altura "))
-A=(l*a)
-P=(2*l)+(2*a)
-print ("El area del rectangulo es de ", A)
-print ("El perimetro del rectangulo es de ", P) 
