e8e8e52a928306c0151c962d08678f7f34746dad
Golfirox <106554979+Golfirox@users.noreply.github.com>
2022-05-30 21:11:42 +0200

    Delete Ejercicio4.py

--- Ejercicio4.py
+++
@@ -1,7 +0,0 @@
-# Dados dos números, mostrar la suma, resta, división y multiplicación de ambos.  
-a= int (input ("Dime el 1er numero "))
-b= int (input ("Dime el 2º numero "))
-print ("La suma es ", a+b)
-print ("La resta es ", a-b)
-print ("La multiplicacion es ", a*b)
-print ("La division es ", a/b)
